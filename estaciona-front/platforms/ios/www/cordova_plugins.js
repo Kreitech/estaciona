afd7e7fee970d46c20ab5eab8c0c2166ef278c7c
Santiago Martinez <smartinez@kreitech.com.uy>
2015-11-06 16:57:27 -0300

    front v1

---
+++ estaciona-front/platforms/ios/www/cordova_plugins.js
@@ -0,0 +1,19 @@
+cordova.define('cordova/plugin_list', function(require, exports, module) {
+module.exports = [
+    {
+        "file": "plugins/plugin.google.maps/www/googlemaps-cdv-plugin.js",
+        "id": "plugin.google.maps.phonegap-googlemaps-plugin",
+        "clobbers": [
+            "plugin.google.maps"
+        ]
+    }
+];
+module.exports.metadata = 
+// TOP OF METADATA
+{
+    "cordova-plugin-whitelist": "1.0.0",
+    "plugin.google.maps": "1.3.4",
+    "com.googlemaps.ios": "1.10.4"
+}
+// BOTTOM OF METADATA
+});
